fdea0bfbd68feb90d606554ff96dde19bffe2d88
Jordan Eldredge <jordan@jordaneldredge.com>
2016-10-13 18:34:35 -0700

    Golf restArgs by using slice

--- underscore.js
+++ underscore.js
@@ -109,21 +109,13 @@
   var restArgs = function(func, startIndex) {
     startIndex = startIndex == null ? func.length - 1 : +startIndex;
     return function() {
-      var length = Math.max(arguments.length - startIndex, 0),
-          rest = Array(length),
-          index = 0;
-      for (; index < length; index++) {
-        rest[index] = arguments[index + startIndex];
-      }
+      var rest = slice.call(arguments, startIndex);
       switch (startIndex) {
         case 0: return func.call(this, rest);
         case 1: return func.call(this, arguments[0], rest);
         case 2: return func.call(this, arguments[0], arguments[1], rest);
       }
-      var args = Array(startIndex + 1);
-      for (index = 0; index < startIndex; index++) {
-        args[index] = arguments[index];
-      }
+      var args = slice.call(arguments, 0, startIndex);
       args[startIndex] = rest;
       return func.apply(this, args);
     };
